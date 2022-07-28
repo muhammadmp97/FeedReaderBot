cdf763455f164c7111ad9d27b98b42b59ca05640
WebPajooh <webpajooh@gmail.com>
2022-07-29 00:06:03 +0430

    feat: /remove command

--- bot.php
+++ bot.php
@@ -34,6 +34,28 @@ try {
             'text' => '✅ Feed added successfully!',
         ]);
     });
+
+    $tg->listen('/remove %p', function ($url) use ($tg, $config, $feeds) {
+        $newFeeds = array_filter($feeds, function ($feed) use ($url) {
+            return $feed->url !== $url;
+        });
+
+        if (count($newFeeds) === count($feeds)) {
+            return $tg->sendMessage([
+                'chat_id' => $config['owner_user_id'],
+                'reply_to_message_id' => $tg->message->message_id,
+                'text' => '❗️ Feed not found!',
+            ]);
+        }
+
+        file_put_contents(__DIR__ . '/config/feeds.json', json_encode(['feeds' => $newFeeds]));
+
+        $tg->sendMessage([
+            'chat_id' => $config['owner_user_id'],
+            'reply_to_message_id' => $tg->message->message_id,
+            'text' => '✅ Feed removed successfully!',
+        ]);
+    });
 } catch (Throwable $th) {
     $tg->sendMessage([
         'chat_id' => $config['owner_user_id'],
